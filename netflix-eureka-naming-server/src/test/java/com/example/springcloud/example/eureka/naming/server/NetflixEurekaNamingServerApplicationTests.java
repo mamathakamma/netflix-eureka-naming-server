df90942bdc68c0914089017615bd634ffff6ee34
mamathakamma <mamatha@hutechsolutions.com>
2023-04-12 15:27:55 +0530

    netflix-eureka-naming-server

---
+++ netflix-eureka-naming-server/src/test/java/com/example/springcloud/example/eureka/naming/server/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.example.eureka.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
